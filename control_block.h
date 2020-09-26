efd6559fbbdd150d2780ea508d030c135f1a3081
pprettysimpple <andrianov.kirill2010@yandex.ru>
2020-09-26 23:06:55 +0300

    tests passed

---
+++ control_block.h
@@ -0,0 +1,116 @@
+//
+// Created by kirill on 9/26/20.
+//
+
+#ifndef SHARED_PTR_TESTING_CONTROL_BLOCK_H
+#define SHARED_PTR_TESTING_CONTROL_BLOCK_H
+
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+struct control_block {
+    control_block() noexcept;
+
+    void add_ref_shared() noexcept;
+
+    void add_ref_weak() noexcept;
+
+    void dec_ref_shared() noexcept;
+
+    void dec_ref_weak() noexcept;
+
+    size_t ref_count_shared() const noexcept;
+
+    size_t ref_count_weak() const noexcept;
+
+    virtual void delete_object() noexcept = 0;
+
+    virtual ~control_block() = default;
+
+private:
+    size_t ref_cnt_shared;
+    size_t ref_cnt_weak;
+};
+
+void control_block::add_ref_shared() noexcept {
+    ++ref_cnt_shared;
+}
+
+void control_block::add_ref_weak() noexcept {
+    ++ref_cnt_weak;
+}
+
+void control_block::dec_ref_shared() noexcept {
+    --ref_cnt_shared;
+}
+
+void control_block::dec_ref_weak() noexcept {
+    --ref_cnt_weak;
+}
+
+size_t control_block::ref_count_shared() const noexcept {
+    return ref_cnt_shared;
+}
+
+size_t control_block::ref_count_weak() const noexcept {
+    return ref_cnt_weak;
+}
+
+control_block::control_block() noexcept: ref_cnt_shared(1), ref_cnt_weak(0) {}
+
+template<typename T, typename D = std::default_delete<T>>
+struct regular_control_block final : control_block {
+    explicit regular_control_block(T* ptr, D deleter = std::default_delete<T>());
+
+    void delete_object() noexcept override;
+
+    ~regular_control_block() noexcept override = default;
+
+private:
+    T* ptr = nullptr;
+    D deleter = std::default_delete<T>();
+};
+
+template<typename T, typename D>
+regular_control_block<T, D>::regular_control_block(T* ptr, D deleter)
+        : control_block(), ptr(ptr), deleter(deleter) {}
+
+template<typename T, typename D>
+void regular_control_block<T, D>::delete_object() noexcept {
+    deleter(ptr);
+}
+
+template<typename T>
+struct inplace_control_block final : control_block {
+    template<typename... Args>
+    explicit inplace_control_block(Args&& ... args);
+
+    T* get() noexcept;
+
+    void delete_object() noexcept override;
+
+    ~inplace_control_block() noexcept override = default;
+
+private:
+    typename std::aligned_storage<sizeof(T), alignof(T)>::type stg;
+};
+
+template<typename T>
+template<typename... Args>
+inplace_control_block<T>::inplace_control_block(Args&& ... args)
+        : control_block() {
+    new(&stg) T(std::forward<Args>(args)...);
+}
+
+template<typename T>
+T* inplace_control_block<T>::get() noexcept {
+    return reinterpret_cast<T*>(&stg);
+}
+
+template<typename T>
+void inplace_control_block<T>::delete_object() noexcept {
+    return reinterpret_cast<T*>(&stg)->~T();
+}
+
+#endif //SHARED_PTR_TESTING_CONTROL_BLOCK_H
--- shared_ptr.h
+++ shared_ptr.h
@@ -1,1 +1,348 @@
 #pragma once
+
+#include "control_block.h"
+
+template<typename T>
+struct shared_ptr;
+
+template<typename T>
+struct weak_ptr {
+    template<typename Friend>
+    friend
+    struct shared_ptr;
+
+    //
+
+    constexpr weak_ptr() noexcept = default;
+
+    weak_ptr(weak_ptr const& r) noexcept;
+
+    weak_ptr(weak_ptr&& r) noexcept;
+
+    weak_ptr& operator=(weak_ptr const& rhs) noexcept;
+
+    weak_ptr& operator=(weak_ptr&& rhs) noexcept;
+
+    //
+
+    template<typename Y>
+    weak_ptr(shared_ptr<Y>& r) noexcept;
+
+    ~weak_ptr() noexcept;
+
+    size_t use_count() const noexcept;
+
+    bool expired() const noexcept;
+
+    shared_ptr<T> lock() const noexcept;
+
+    void swap(weak_ptr& rhs) noexcept;
+
+private:
+    control_block* cblock = nullptr;
+    T* ptr = nullptr;
+};
+
+template<typename T>
+weak_ptr<T>::weak_ptr(weak_ptr const& r) noexcept
+        : cblock(r.cblock), ptr(r.ptr) {
+    if (cblock == nullptr) {
+        return;
+    }
+    cblock->add_ref_weak();
+}
+
+template<typename T>
+weak_ptr<T>::weak_ptr(weak_ptr&& r) noexcept {
+    r.swap(*this);
+    weak_ptr().swap(r);
+}
+
+template<typename T>
+weak_ptr<T>& weak_ptr<T>::operator=(weak_ptr const& rhs) noexcept {
+    weak_ptr(rhs).swap(*this);
+    return *this;
+}
+
+template<typename T>
+weak_ptr<T>& weak_ptr<T>::operator=(weak_ptr&& rhs) noexcept {
+    weak_ptr(std::move(rhs)).swap(*this);
+    return *this;
+}
+
+template<typename T>
+template<typename Y>
+weak_ptr<T>::weak_ptr(shared_ptr<Y>& r) noexcept
+        : cblock(r.cblock), ptr(r.ptr) {
+    cblock->add_ref_weak();
+}
+
+template<typename T>
+weak_ptr<T>::~weak_ptr() noexcept {
+    if (cblock == nullptr) {
+        return;
+    }
+    cblock->dec_ref_weak();
+    if (cblock->ref_count_weak() == 0 && cblock->ref_count_shared() == 0) {
+        delete cblock;
+    }
+}
+
+template<typename T>
+size_t weak_ptr<T>::use_count() const noexcept {
+    return cblock == nullptr ? 0 : cblock->ref_count_shared();
+}
+
+template<typename T>
+bool weak_ptr<T>::expired() const noexcept {
+    return use_count() == 0;
+}
+
+template<typename T>
+shared_ptr<T> weak_ptr<T>::lock() const noexcept {
+    return expired() ? shared_ptr<T>() : shared_ptr<T>(*this);
+}
+
+template<typename T>
+void weak_ptr<T>::swap(weak_ptr& rhs) noexcept {
+    using std::swap;
+    swap(cblock, rhs.cblock);
+    swap(ptr, rhs.ptr);
+}
+
+template<typename T>
+struct shared_ptr {
+    template<typename Friend>
+    friend
+    struct weak_ptr;
+
+    template<typename Friend>
+    friend
+    struct shared_ptr;
+
+    constexpr shared_ptr() noexcept = default;
+
+    constexpr explicit shared_ptr(std::nullptr_t) noexcept;
+
+    template<typename Y>
+    explicit shared_ptr(Y* ptr);
+
+    template<typename Y, typename D>
+    shared_ptr(Y* ptr, D deleter);
+
+    template<typename Y>
+    shared_ptr(shared_ptr<Y> const& rhs) noexcept;
+
+    //
+
+    shared_ptr(shared_ptr const& rhs) noexcept;
+
+    shared_ptr(shared_ptr&& rhs) noexcept;
+
+    shared_ptr& operator=(shared_ptr const& rhs) noexcept;
+
+    shared_ptr& operator=(shared_ptr&& rhs) noexcept;
+
+    //
+
+    template<class Y>
+    shared_ptr(shared_ptr<Y> const& r, T* ptr) noexcept;
+
+    template<class Y>
+    shared_ptr(weak_ptr<Y> const& r);
+
+    ~shared_ptr();
+
+    T* get() const noexcept;
+
+    T& operator*() const noexcept;
+
+    T* operator->() const noexcept;
+
+    size_t use_count() const noexcept;
+
+    explicit operator bool() const noexcept;
+
+    void reset() noexcept;
+
+    template<typename Y>
+    void reset(Y* rhs) noexcept;
+
+    template<class Y, class Deleter>
+    void reset(Y* ptr, Deleter d);
+
+    void swap(shared_ptr&) noexcept;
+
+private:
+    control_block* cblock = nullptr;
+    T* ptr = nullptr;
+
+    template<typename U, typename... Args>
+    friend shared_ptr<U> make_shared(Args&& ... args);
+};
+
+template<typename T>
+template<typename Y>
+shared_ptr<T>::shared_ptr(shared_ptr<Y> const& rhs) noexcept
+        : cblock(rhs.cblock), ptr(rhs.ptr) {
+    if (rhs == nullptr) {
+        return;
+    }
+    cblock->add_ref_shared();
+}
+
+template<typename T>
+shared_ptr<T>::shared_ptr(shared_ptr const& rhs) noexcept
+        : cblock(rhs.cblock), ptr(rhs.ptr) {
+    if (rhs == nullptr) {
+        return;
+    }
+    cblock->add_ref_shared();
+}
+
+template<typename T, typename... Args>
+shared_ptr<T> make_shared(Args&& ... args) {
+    auto* cblock = new inplace_control_block<T>(std::forward<Args>(args)...);
+    shared_ptr<T> cur;
+    cur.cblock = cblock;
+    cur.ptr = cblock->get();
+    return cur;
+}
+
+template<typename T>
+template<typename Y>
+shared_ptr<T>::shared_ptr(Y* p)
+        : cblock(new regular_control_block<Y>(p)), ptr(p) {}
+
+template<typename T>
+template<typename Y, typename D>
+shared_ptr<T>::shared_ptr(Y* p, D deleter)
+        : cblock(new regular_control_block(p, deleter)), ptr(p) {}
+
+template<typename T>
+shared_ptr<T>::operator bool() const noexcept {
+    return bool(ptr);
+}
+
+template<typename T>
+template<class Y>
+shared_ptr<T>::shared_ptr(shared_ptr<Y> const& r, T* ptr) noexcept
+        : cblock(r.cblock), ptr(ptr) {
+    cblock->add_ref_shared();
+}
+
+template<typename T>
+constexpr shared_ptr<T>::shared_ptr(std::nullptr_t) noexcept {}
+
+template<typename T>
+shared_ptr<T>::shared_ptr(shared_ptr&& rhs) noexcept {
+    rhs.swap(*this);
+    shared_ptr().swap(rhs);
+}
+
+template<typename T>
+template<class Y>
+shared_ptr<T>::shared_ptr(weak_ptr<Y> const& r)
+        : cblock(r.cblock), ptr(r.ptr) {
+    cblock->add_ref_shared();
+}
+
+template<typename T>
+shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr const& rhs) noexcept {
+    shared_ptr(rhs).swap(*this);
+    return *this;
+}
+
+template<typename T>
+shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr&& rhs) noexcept {
+    shared_ptr(std::move(rhs)).swap(*this);
+    return *this;
+}
+
+template<typename T>
+T* shared_ptr<T>::get() const noexcept {
+    return ptr;
+}
+
+template<typename T>
+T& shared_ptr<T>::operator*() const noexcept {
+    return *get();
+}
+
+template<typename T>
+T* shared_ptr<T>::operator->() const noexcept {
+    return get();
+}
+
+template<typename T>
+size_t shared_ptr<T>::use_count() const noexcept {
+    return cblock == nullptr ? 0 : cblock->ref_count_shared();
+}
+
+template<typename T>
+void shared_ptr<T>::swap(shared_ptr& rhs) noexcept {
+    using std::swap;
+    swap(ptr, rhs.ptr);
+    swap(cblock, rhs.cblock);
+}
+
+template<typename T>
+void shared_ptr<T>::reset() noexcept {
+    shared_ptr().swap(*this);
+}
+
+template<typename T>
+template<typename Y>
+void shared_ptr<T>::reset(Y* rhs) noexcept {
+    shared_ptr<T>(rhs).swap(*this);
+}
+
+template<typename T>
+template<class Y, class Deleter>
+void shared_ptr<T>::reset(Y* p, Deleter d) {
+    shared_ptr<T>(p, d).swap(*this);
+}
+
+template<typename T>
+shared_ptr<T>::~shared_ptr() {
+    if (cblock == nullptr) {
+        return;
+    }
+    cblock->dec_ref_shared();
+    if (cblock->ref_count_shared() == 0) {
+        cblock->delete_object();
+        if (cblock->ref_count_weak() == 0) {
+            delete cblock;
+        }
+    }
+}
+
+template<typename V, typename U>
+bool operator==(shared_ptr<V> const& lhs, shared_ptr<U> const& rhs) {
+    return lhs.get() == rhs.get();
+}
+
+template<typename V, typename U>
+bool operator!=(shared_ptr<V> const& lhs, shared_ptr<U> const& rhs) {
+    return lhs.get() != rhs.get();
+}
+
+template<typename V>
+bool operator==(shared_ptr<V> const& lhs, std::nullptr_t) {
+    return lhs.get() == nullptr;
+}
+
+template<typename V>
+bool operator==(std::nullptr_t, shared_ptr<V> const& lhs) {
+    return lhs.get() == nullptr;
+}
+
+template<typename V>
+bool operator!=(shared_ptr<V> const& lhs, std::nullptr_t) {
+    return lhs.get() != nullptr;
+}
+
+template<typename V>
+bool operator!=(std::nullptr_t, shared_ptr<V> const& lhs) {
+    return lhs.get() != nullptr;
+}
